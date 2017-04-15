17b5a10a9cd81a1653111b035d8a03e7e15ac53b
Arthur Asatryan <biacoder@gmail.com>
2017-04-15 17:15:59 +0400

    #9 Added basic diesel setup and some demo code

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,30 @@
 
 extern crate rocket;
 
+extern crate rustyblog;
+extern crate diesel;
+
+use self::rustyblog::*;
+use self::rustyblog::models::*;
+use self::diesel::prelude::*;
+
 #[get("/")]
 fn index() -> &'static str {
+    use rustyblog::schema::posts::dsl::*;
+
+    let connection = establish_connection();
+    let results = posts.filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}\n", post.body);
+    }
+
     "Welcome to RustyBlog!!!"
 }
 
