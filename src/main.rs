8fc8c5e4240486f5ba1534d9062e8dfbd9fcb3f3
Arthur Asatryan <biacoder@gmail.com>
2017-04-16 03:24:57 +0400

    #10 Changed some travis confs for postgresql

--- src/main.rs
+++ src/main.rs
@@ -39,11 +39,9 @@ fn foo() {
 
     println!("What would you like your title to be?");
     let mut title = String::new();
-    stdin().read_line(&mut title).unwrap();
     let title = &title[..(title.len() - 1)]; // Drop the newline character
     println!("\nOk! Let's write {} (Press {} when finished)\n", title, EOF);
     let mut body = String::new();
-    stdin().read_to_string(&mut body).unwrap();
 
     let post = create_post(&connection, title, &body);
     println!("\nSaved draft {} with id {}", title, post.id);
--- src/schema.rs
+++ src/schema.rs
@@ -1,2 +1,1 @@
-#[cfg(not(test))]
 infer_schema!("dotenv:DATABASE_URL");
